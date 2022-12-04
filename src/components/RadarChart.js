0ad7a80f020d934f518c612ca5a147202f1bbec3
Tyler Liddicoat <tylerlidd@sbcglobal.net>
2022-12-03 18:52:46 -0800

    fixing radar chart

--- src/components/RadarChart.js
+++ src/components/RadarChart.js
@@ -1,4 +1,5 @@
 import { Radar } from 'react-chartjs-2';
+import {Chart as ChartJS, Scale} from 'chart.js/auto'
 
 const RadarChart = ({chartData, options}) => {
   return <Radar data={chartData} options={options}/>
