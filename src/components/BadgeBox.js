3d9a3567b2a388145b60bca3fd9c3fe71910f06d
Tyler Liddicoat <tylerlidd@sbcglobal.net>
2022-12-15 18:36:51 -0800

    added a legend to the badge box

--- src/components/BadgeBox.js
+++ src/components/BadgeBox.js
@@ -7,6 +7,7 @@ const BadgeBox = ({badges}) => {
 
     let badgeValues = [];
 
+    //insert the values for the badges in the badge box and order it by highest badges
     Object.keys(badgeNames).map((key, index) => {
         if(key in badges){
             badgeValues[index] = [key, badges[key]];
@@ -14,8 +15,6 @@ const BadgeBox = ({badges}) => {
             badgeValues[index] = [key, -1];
         }
     })
-
-    console.log(badgeValues);
 
     badgeValues.sort((a, b) => {
         if(a[1] > b[1]) return -1;
@@ -26,14 +25,31 @@ const BadgeBox = ({badges}) => {
     
   return (
     <div className="badge-box-container">
-        <div className="box-scroll">
-            {
-                badgeValues.map(badge => {
-                    return <div className="box-scroll-item" key={badgeNames[badge[0]]}>
-                            <Badge badgeImage={badge[0]} badgeName={badgeNames[badge[0]]} showBadgeText={true} level={badge[1]}/>
-                        </div>
-                })
-            }
+        <div className="badge-box-scroll-container">
+            <div className="box-scroll">
+                {
+                    badgeValues.map(badge => {
+                        return <div className="box-scroll-item" key={badgeNames[badge[0]]}>
+                                <Badge badgeImage={badge[0]} badgeName={badgeNames[badge[0]]} showBadgeText={true} level={badge[1]}/>
+                            </div>
+                    })
+                }
+            </div>
+        </div>
+        {/* a legend for the box so users understand the values of the badges */}
+        <div className="badge-box-legend">
+            <div className="legend-item">
+                <div className="level-square Excellent1" />
+                <div className="level-name">Excellent</div>
+            </div>
+            <div className="legend-item">
+                <div className="level-square Great1" />
+                <div className="level-name">Great</div>
+            </div>
+            <div className="legend-item">
+                <div className="level-square Good1" />
+                <div className="level-name">Good</div>
+            </div>
         </div>
     </div>
   )
