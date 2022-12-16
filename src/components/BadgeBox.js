dab4f2a9a03e502c2874e82c4084bae450716e96
Tyler Liddicoat <tylerlidd@sbcglobal.net>
2022-12-15 19:18:48 -0800

    added a badge description box

--- src/components/BadgeBox.js
+++ src/components/BadgeBox.js
@@ -24,34 +24,41 @@ const BadgeBox = ({badges}) => {
     })
     
   return (
-    <div className="badge-box-container">
-        <div className="badge-box-scroll-container">
-            <div className="box-scroll">
-                {
-                    badgeValues.map(badge => {
-                        return <div className="box-scroll-item" key={badgeNames[badge[0]]}>
-                                <Badge badgeImage={badge[0]} badgeName={badgeNames[badge[0]]} showBadgeText={true} level={badge[1]}/>
-                            </div>
-                    })
-                }
+    <>
+        <div className="badge-box-container">
+            <div className="badge-box-scroll-container">
+                <div className="box-scroll">
+                    {
+                        badgeValues.map(badge => {
+                            return <div className="box-scroll-item" key={badgeNames[badge[0]]}>
+                                    <Badge badgeImage={badge[0]} badgeName={badgeNames[badge[0]]} showBadgeText={true} level={badge[1]}/>
+                                </div>
+                        })
+                    }
+                </div>
+            </div>
+            {/* a legend for the box so users understand the values of the badges */}
+            <div className="badge-box-legend">
+                <div className="legend-item">
+                    <div className="level-square Excellent1" />
+                    <div className="level-name">Excellent</div>
+                </div>
+                <div className="legend-item">
+                    <div className="level-square Great1" />
+                    <div className="level-name">Great</div>
+                </div>
+                <div className="legend-item">
+                    <div className="level-square Good1" />
+                    <div className="level-name">Good</div>
+                </div>
             </div>
         </div>
-        {/* a legend for the box so users understand the values of the badges */}
-        <div className="badge-box-legend">
-            <div className="legend-item">
-                <div className="level-square Excellent1" />
-                <div className="level-name">Excellent</div>
-            </div>
-            <div className="legend-item">
-                <div className="level-square Great1" />
-                <div className="level-name">Great</div>
-            </div>
-            <div className="legend-item">
-                <div className="level-square Good1" />
-                <div className="level-name">Good</div>
-            </div>
+        <div className="badge-box-description">
+            Badges are gained by outperforming players in your games. If you perform in the top percentage for the badge category, you 
+            will receive a <b>Excellent</b> badge. Below that is the <b>Great</b> badge, and if you perform slightly above average in your games you
+            will receive the <b>Good</b> badge.
         </div>
-    </div>
+    </>
   )
 }
 
