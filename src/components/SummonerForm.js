e880c242a85226ea050e70b392d9340b58a23f70
Tyler Liddicoat <tylerlidd@sbcglobal.net>
2023-01-12 15:06:25 -0800

    added a region select drop down to choose which region the player can search for

--- src/components/SummonerForm.js
+++ src/components/SummonerForm.js
@@ -32,7 +32,38 @@ const SummonerForm = () => {
             <div className="form-container">
                 <form onSubmit={formSubmit}>
                     <label htmlFor="summonerName">Summoner Name:</label>
-                    <input className="textInput" maxLength={16} style={{"fontSize":"25px"}} type="text" id="summonerName" ref={textInput}></input>
+                    {/* <input className="textInput" maxLength={16} style={{"fontSize":"18px"}} type="text" id="summonerName" ref={textInput}></input>
+                    <select >
+                        <option value={"NA"}>NA</option>
+                        <option value={"NA"}>EUW</option>
+                        <option value={"NA"}>EUNE</option>
+                        <option value={"NA"}>OC</option>
+                        <option value={"NA"}>KR</option>
+                        <option value={"NA"}>JP</option>
+                        <option value={"NA"}>BR</option>
+                        <option value={"NA"}>LA1</option>
+                        <option value={"NA"}>LA2</option>
+                        <option value={"NA"}>RU</option>
+                        <option value={"NA"}>TR</option>
+                        <option value={"NA"}>CN</option>
+                    </select> */}
+                    <div className="input-box">
+                        <input className="text-input" maxLength={16} style={{"fontSize":"18px"}} type="text" id="summonerName" ref={textInput}></input>
+                        <select>
+                            <option value={"NA"}>NA</option>
+                            <option value={"NA"}>EUW</option>
+                            <option value={"NA"}>EUNE</option>
+                            <option value={"NA"}>OC</option>
+                            <option value={"NA"}>KR</option>
+                            <option value={"NA"}>JP</option>
+                            <option value={"NA"}>BR</option>
+                            <option value={"NA"}>LA1</option>
+                            <option value={"NA"}>LA2</option>
+                            <option value={"NA"}>RU</option>
+                            <option value={"NA"}>TR</option>
+                            <option value={"NA"}>CN</option>
+                        </select>
+                    </div>
                     <input className="button" type="submit" value="Find Summoner"></input>
                 </form>
                 {isPending && formName.length >= 3 && <img className="loadingGif" src={require("../assets/loading.gif")} alt="loading..."/>}
