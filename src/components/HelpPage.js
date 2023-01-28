79b05f487514ebe5b8e832d45c810f608ef8dad3
Tyler Liddicoat <tylerlidd@sbcglobal.net>
2023-01-28 14:47:46 -0800

    redid the css in HelpPage with a mobile first development method in mind to clean it up

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -3,24 +3,26 @@ import "../Stylesheets/HelpPage.css"
 
 const HelpPage = () => {
   return (
-    <>
+    <div>
         <NavIcon />
-        <h1>HOW IT WORKS</h1>
-        <div className="help-container">
-            <div className="help-item">
-                <p>Enter in a summoner name to search <br/> to receive player info.</p>
-                <img className="help-img" src={require("../assets/help1.png")} alt=""></img>
-            </div>
-            <div className="help-item">
-                <img className="help-img" src={require("../assets/help2.png")} alt=""></img>
-                <p>See current game lobby and what each player excels at by reviewing their badges.</p>
-            </div>
-            <div className="help-item">
-                <p>Review a player individually to get more <br/> information on their performances.</p>
-                <img className="help-img" src={require("../assets/help3.png")} alt=""></img>
+        <div className="help-page-container">
+            <h1>HOW IT WORKS</h1>
+            <div className="help-container">
+                <div className="help-item">
+                    <p>Enter in a summoner name to search to receive player info.</p>
+                    <img className="help-img" src={require("../assets/help1.png")} alt=""></img>
+                </div>
+                <div className="help-item">
+                    <p>See current game lobby and what each player excels at by reviewing their badges.</p>
+                    <img className="help-img" src={require("../assets/help2.png")} alt=""></img>
+                </div>
+                <div className="help-item">
+                    <p>Review a player individually to get more <br/> information on their performances.</p>
+                    <img className="help-img" src={require("../assets/help3.png")} alt=""></img>
+                </div>
             </div>
         </div>
-    </>
+    </div>
   )
 }
 
